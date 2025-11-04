02158278a571038f53f7cc9fdd4f4732de3e6d7c
margaretpalma <mpalma1496@gmail.com>
2025-11-04 10:57:43 -0500

    "person" runs

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,5 @@
 package com.pluralsight;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +8,40 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         List<Person> people = new ArrayList<>();
-        // people.add(//new person ("name" "lasstName" "age"))
+        people.add(new Person("Ezra", "Cooper", 4));
+        people.add(new Person("Leah", "London", 11));
+        people.add(new Person("Emma", "D'Arcy", 32));
+        people.add(new Person("Kristen", "Stewart", 25));
+        people.add(new Person("Mia", "Dot", 22));
+        people.add(new Person("Robert", "Pattison", 38));
+        people.add(new Person("Leah", "Hendrix", 27));
+        people.add(new Person("Ezra", "Black", 30));
+        people.add(new Person("Clancy", "Stewart", 40));
+        people.add(new Person("Dominic", "Dot", 19));
 
+        while(true){
+            String searchForName = ConsoleHelper.promptForString("Enter First Or Last Name").toLowerCase();
 
+            if(searchForName.equals("quit")){
+                System.out.println("Exiting Program");
+                break;
+            }
+
+            //stream - turns list into stream - person one by one
+            List<Person> personMatches = people.stream()
+                    //keep matching ones
+                    .filter(person ->
+                            person.getFirstName().equalsIgnoreCase(searchForName) ||
+                                    person.getLastName().equalsIgnoreCase(searchForName))
+
+                    //turn stream into list
+                    .toList();
+
+            if(personMatches.isEmpty()) {
+                System.out.println("No Matches Found For: " + searchForName);
+            }else {
+                personMatches.forEach(person -> System.out.println(person));
+            }
+        }
     }
 }
--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -35,4 +35,13 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Person---" +
+                " First Name - " + firstName +
+                "| Last Name - " + lastName +
+                "| Age - "  + age +
+                '|';
+    }
 }
